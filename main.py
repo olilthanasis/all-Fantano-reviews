93a834251b4d8c85a4916499be7952ee6fe72156
lilthanasis <53539630+olilthanasis@users.noreply.github.com>
2020-08-29 16:48:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ token = None
 while True:
     try:
         res = youtube.playlistItems().list(playlistId=uploads, part='snippet', maxResults=50, pageToken=token).execute()
+        print(res)
         for i in res['items']:
             dict[i['snippet']['title']] = i['snippet']['description']
         token = res['nextPageToken']
@@ -32,14 +33,13 @@ with open('ratings.txt', "rb") as file:
     all = file.read().decode("utf-8")
     all = all.split("|||")
     all = all[:-1]
+    print(type(all))
     ratings_dict = {}
+    print(len(all))
     for n, i in enumerate(all):
         if n % 2 == 0:
-            rating = re.findall("10/10", all[n + 1])
+            rating = re.findall("10/10|[0,1,2,3,4,5,6,7,8,9]/10", all[n + 1])
             if rating:
-                ratings_dict[i] = rating
-            if not rating:
-                rating = re.findall("\d/10", all[n + 1])
                 ratings_dict[i] = rating
             else:
                 ratings_dict[i] = "no rating found in video description"
@@ -47,3 +47,4 @@ with open('ratings.txt', "rb") as file:
     f = open("results.txt", "wb")
     for i in ratings_dict:
         f.write(f"{i}, {ratings_dict[i]}\n".encode("utf-8"))
+    f.close()
